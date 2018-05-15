fdbbd67b6a07c23ca26d05e4f46ddbb7b7e07936
Luc Comeau <comeaul@byui.edu>
2018-05-15 10:44:58 -0600

    updates

--- week 03/team03.py
+++ week 03/team03.py
@@ -1,9 +1,10 @@
 # *******************************************************************
 # 03: CS101 Team Activity
 # 
-# These are simple coding problems.  You will not be submitted anything
+# These are simple coding problems.  You will not be submitting anything
 # for points.  Please work together on this.  Add your code for every
 # TODO section.  Copy this code to Thonny, or your python system.
+# Use the print() function to get the output that is in the double quotes.
 # *******************************************************************
 
 # TODO -> "Hello World"
@@ -25,7 +26,7 @@ name2 = 'Brown'
 age = 34
 
 
-# TODO -> "ABC" 
+# TODO -> "ABC", change the '?' string to get the output using {}
 print('?'.format('A', 'B', 'C'))
 
 
@@ -56,7 +57,7 @@ age = 34
 
 # TODO -> Calculate the average of the following variables
 #         Use the int() funtion.
-#         "The average of [123, 23, 13] is 54"
+#         "The average of [123, 23, 13] is 54.0"
 value1 = '123'
 value2 = '23'
 value3 = '16'
