1cf88cf7e355e70e578d20d6852e7c48cdb5264e
Luc Comeau <comeaul@byui.edu>
2018-05-17 10:27:47 -0600

    Team Activity

---
+++ week 04/team04.py
@@ -0,0 +1,48 @@
+# *******************************************************************
+# 04: CS101 Team Activity
+# 
+# These are simple coding problems.  You will not be submitting anything
+# for points.  Please work together on this.  Add your code for every
+# TODO section.  Copy this code to Thonny, or your python system.
+# Use the print() function to get the output that is in the double quotes.
+#
+# This team activity is on Boolean Expressions and if statements
+# *******************************************************************
+
+# -------------------------------------------------------------------
+# Convert US to Euro
+#
+# Write a program to request a dollar amount from the user and convert
+# it to Euros.  Find the current change rate from the Internet and use
+# it in your program
+#
+# Example output of your program where the user enters 100.0 dollars
+#
+# Enter dollars: 100.0
+# 100.0 dollars is 85.0 Euros
+# -------------------------------------------------------------------
+
+# TODO Add your code here
+
+
+# -------------------------------------------------------------------
+# Compute tax rate
+#
+# Write a program to request yearly income from the user.  Your program
+# will calculate the tax rate for that income
+#
+# Tax Rates:
+#
+# Income                  Rate
+# 0.0 up to 10000.0        10
+# 10000.0 up to 20000.0    20
+# 20000.0 up to 100000.0   30
+# over 100000.0            35
+#
+# Example output of your program where the user enters income of 12345.00
+#
+# Enter income: 12345.00
+# Your tax rate is 20%
+# -------------------------------------------------------------------
+
+# TODO Add your code here
