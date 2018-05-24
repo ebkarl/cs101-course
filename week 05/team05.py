8050388867a5369aaf5692c0a60b85b7700b4e17
Luc Comeau <comeaul@byui.edu>
2018-05-24 09:28:03 -0600

    Team Activity

---
+++ week 05/team05.py
@@ -0,0 +1,50 @@
+# *******************************************************************
+# 05: CS101 Team Activity
+# 
+# These are simple coding problems.  You will not be submitting anything
+# for points.  Please work together on this.  Add your code for every
+# TODO section.  Copy this code to Thonny, or your python system.
+# Use the print() function to get the output that is in the double quotes.
+#
+# This team activity is on Boolean Expressions and if statements
+# *******************************************************************
+
+# -------------------------------------------------------------------
+# Convert Fahrenheit to Celsius
+#
+# Write a program to request Fahrenheit and convert it to Celsius.
+# You will be using functions to write this program.  Below is what
+# the main "function" will be do, your task to the write the functions
+# that main requires.
+#
+# You should not change any of the code in main.
+#
+# Example output of your program
+#
+# Run 1:
+#
+# Welcome to this program that will Convert Fahrenheit to Celsius
+#
+# Enter Fahrenheit: 212
+# 212 Fahrenheit is 100.0 Celsius
+# 
+#
+# Run 2:
+#
+# Welcome to this program that will Convert Fahrenheit to Celsius
+#
+# Enter Fahrenheit: -40
+# (-40) Fahrenheit is (-40) Celsius
+# -------------------------------------------------------------------
+
+# TODO Add your functions before the main code
+
+
+
+
+# Main function code
+
+displayWelcome()
+fahrenheit = getFahrenheit()
+celsius = convertFtoC(fahrenheit)
+displayResults(fahrenheit, celsius)
