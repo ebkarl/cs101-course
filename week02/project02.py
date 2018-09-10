f6906a2679f81a5f85305310e48467eb818b3942
Luc Comeau <comeaul@byui.edu>
2018-09-10 16:05:43 -0600

    Week 02

---
+++ week02/project02.py
@@ -0,0 +1,18 @@
+"""
+Course: CS101
+File: <name of your file>
+Project: <project number>
+Author: <Your name>
+
+Description:
+  <What does your project/program do?>
+"""
+
+"""
+Instructions:
+
+Write code to display a pizza menu that a business would use.
+"""
+
+# Add your code here
+
---
+++ week02/team02.py
@@ -0,0 +1,100 @@
+"""
+Course: CS101
+File: team02.py
+Author: Brother Comeau
+
+Description:
+  This is the code for the weekly team activity.  
+  Please work together in groups of 2 to 3.  
+  You will not be sumbitting your code for this activity.
+  You are free to continue working on this activity after class if you need more time.
+"""
+
+# TODO -> display "Hello World" using one print() statement
+
+
+# TODO -> display "Hello World" using two print() statements
+
+
+# TODO -> display "Hello World is length 11", using the len() function
+
+
+# TODO -> display "ABC", change the '?' string to get the output using {}
+print('?'.format('A', 'B', 'C'))
+
+
+# TODO -> Complete the following to display "CBA"
+print('?'.format('A', 'B', 'C'))
+
+
+# TODO -> Complete the following to display "ABCBA" 
+print('?'.format('A', 'B', 'C'))
+
+
+# TODO -> Complete the following to display "AABBCC"
+print('?'.format('A', 'B', 'C'))
+
+
+# TODO -> Complete the following to display "John"
+print('?'.format('J', 'h', 'n', 'o'))
+
+
+# TODO -> Display your name and your height using the format() statement
+# Example: My name is John Brown and I'm 5' 8"
+
+
+# TODO -> Display a small tree of height 7 using '*' characters
+"""
+Here is an example of a tree of height 3
+    *
+   ***
+  *****
+    *
+"""
+
+
+# TODO -> Display the following using left aligned, centered and right aligned 
+# features of the format() function.
+"""
+Example output:
+
+************ Options ************
+*                               *
+*          1 - Display          *
+*         2 - Play game         *
+*       3 - Exit and Save       *
+*                               *
+*********************************
+
+"""
+
+
+# TODO -> Display the following square
+"""
+Example Output:
+
+╔════════════╗
+║            ║
+║            ║
+║            ║
+║            ║
+╚════════════╝
+
+"""
+
+
+# -------------------------------------------------------------------
+# Extra Challenge
+
+# TODO -> Calculate the average of the following variables
+#         Use the int() funtion.
+#         "The average of [123, 23, 13] is 54.0"
+value1 = '123'
+value2 = '23'
+value3 = '16'
+
+
+# TODO -> Display "John Brown" using the following variables
+name1 = 'john'
+name2 = 'brown'
+
