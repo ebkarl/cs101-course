83d748b8adb8f9dadab5ae7a0116735f78a3bdaf
Luc Comeau <comeaul@byui.edu>
2018-10-25 12:52:27 -0600

    Team Solution

---
+++ week06/team-solution.py
@@ -0,0 +1,133 @@
+"""
+Course: CS101
+File: team02.py
+Author: Brother Comeau
+
+Description:
+  This is the code for the weekly team activity.  
+  Please work together in groups of 2 to 3.  
+  You will not be sumbitting your code for this activity.
+  You are free to continue working on this activity after class if you need more time.
+
+
+Sample Output:
+
+Section 1 --------------------------------------------------------
+Name = John Brown
+Age = 23
+Eye color = Blue
+Name = John Brown
+Age = 23
+Eye color = Blue
+Section 2 --------------------------------------------------------
+Enter Name: John Brown
+Name = John Brown
+Enter your first name: John
+Name = John
+Enter Fahrenheit: 97
+97.00 Fahrenheit is 36.11 Celsius
+Section 3 --------------------------------------------------------
+Sum of 11, 43, 77 = 131
+Product of 11, 43, 77 = 36421
+Max of 11, 43, 77 = 77
+Min of 11, 43, 77 = 11
+Average of 11, 43, 77 = 43.67
+
+"""
+
+# TODO -> Add your functions here.
+
+def displayName(name):
+  print('Name =', name)
+
+def displayAge(age):
+  print('Age =', age)
+
+
+def displayEyeColor(eyeColor):
+  print('Eye color =', eyeColor)
+
+
+def displayPerson(name, age, eyeColor):
+  displayName(name)
+  displayAge(age)
+  displayEyeColor(eyeColor)
+
+def getName(prompt):
+  return(input(prompt))
+
+def getFahrenheit():
+  return(float(input('Enter Fahrenheit: ')))
+
+
+def convertToCelsius(fahrenheit):
+  return ((fahrenheit - 32.0) * 5.0 / 9.0)
+
+def calcSum(num1, num2, num3):
+  return (num1 + num2 + num3)
+
+def calcProduct(num1, num2, num3):
+  return (num1 * num2 * num3)
+
+
+def calcMaxValue(num1, num2, num3):
+  return max(num1, num2, num3)
+
+
+def calcMinValue(num1, num2, num3):
+  return min(num1, num2, num3)
+
+
+def calcAverage(num1, num2, num3):
+  return ((num1 + num2 + num3) / 3.0)
+
+
+# Don't change the following code except for un-commenting out a function
+# at a time as you implement these functions.
+
+print('Section 1 --------------------------------------------------------')
+
+name = 'John Brown'
+age = 23
+eyeColor = 'Blue'
+
+displayName(name)
+displayAge(age)
+displayEyeColor(eyeColor)
+
+displayPerson(name, age, eyeColor)
+
+
+print('Section 2 --------------------------------------------------------')
+
+name = getName('Enter Name: ')
+displayName(name)
+
+name = getName('Enter your first name: ')
+displayName(name)
+
+fahrenheit = getFahrenheit()
+celsius = convertToCelsius(fahrenheit)
+print('{:0.2f} Fahrenheit is {:0.2f} Celsius'.format(fahrenheit, celsius))
+
+print('Section 3 --------------------------------------------------------')
+
+num1 = 11
+num2 = 43
+num3 = 77
+
+total = calcSum(num1, num2, num3)
+print('Sum of {0}, {1}, {2} = {3}'.format(num1, num2, num3, total))
+
+product = calcProduct(num1, num2, num3)
+print('Product of {0}, {1}, {2} = {3}'.format(num1, num2, num3, product))
+
+maxValue = calcMaxValue(num1, num2, num3)
+print('Max of {0}, {1}, {2} = {3}'.format(num1, num2, num3, maxValue))
+
+minValue = calcMinValue(num1, num2, num3)
+print('Min of {0}, {1}, {2} = {3}'.format(num1, num2, num3, minValue))
+
+average = calcAverage(num1, num2, num3)
+print('Average of {0}, {1}, {2} = {3:0.2f}'.format(num1, num2, num3, average))
+
