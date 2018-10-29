5c7cdd8d701bac319e92afaa4a57ee0742f71744
Luc Comeau <comeaul@byui.edu>
2018-10-29 11:42:39 -0600

    Week 09 and 10

--- week10/project.py
+++ week10/project.py
@@ -11,8 +11,51 @@ Description:
 """
 Instructions:
 
-TBA
+Your project will simulate rolling 5 dice and count how many times
+the program needs to roll to get a Yathzee (ie., all 5 dice the same)
+
+1) You are given the function getRoll() that will return 5 dice values
+   that you will use in the program.  Call this function as many times
+   as you need.
+
+2) You must implement the function isYathzee().  This function will return
+   True if the 5 dice make a Yathzee, otherwise return False.
+
+3) Write code to use getRoll() and isYathzee() to solve this project.
+
+4) Calculate the average number of rolls required to get Yathzze.  To calculate
+   this average, simulate 1,000 "runs".
+
+   For example:
+      Run 1 takes 2013 rolls to get Yathzee
+      Run 2 takes 981 rolls to get Yathzee
+      Run 3 takes 1655 rolls to get Yathzee
+                      :
+      Run 1000 takes 3601 rolls to get Yathzee
+
+      Add up the rolls in each run and average the results
+
+5) You are free to create other functions in your project as you need them.
+
+Sample Output:
+
+Number of rolls to get a Yathzee = 941
+Average rolls to get a Yathzee (1000 runs) = 1277.204
+
 """
 
-# Add your code here
+import random
+
+def getRoll():
+  dice1 = random.randint(1, 6)
+  dice2 = random.randint(1, 6)
+  dice3 = random.randint(1, 6)
+  dice4 = random.randint(1, 6)
+  dice5 = random.randint(1, 6)
+  return (dice1, dice2, dice3, dice4, dice5)
+
+# Add your code below
+
+def isYathzee(d1, d2, d3, d4, d5):
+  pass
 
--- week10/team.py
+++ week10/team.py
@@ -10,4 +10,54 @@ Description:
   You are free to continue working on this activity after class if you need more time.
 """
 
-# TBA
+import random
+
+
+# TODO -> Display numbers 4 to 10 using a while loop
+
+
+
+# TODO -> Display the sum of the numbers from 1 to 1,000,000 using a for loop
+
+
+
+# TODO -> Display the sum of the numbers from 1 to 1,000,000 using a while loop
+
+
+
+# TODO -> Write code to request someone's age.  The age must be between 0 and 120.
+#         Continue requesting their age until correct.
+
+
+
+# TODO -> Write code to request someone's ZIP code.  ZIP code must be 5 digits.
+#         Continue requesting the ZIP code until correct.
+
+
+
+# TODO -> Write code to request a number beween 1 and 1,000,000 and it
+#         must contain at least 3 fours in it. For Example: 123456 is bad.
+#         142434 is a good number. 10444444 is bad (too large)
+#         Continue requesting the number until correct.
+#         Hint:  There is a function called count() for strings
+
+
+
+# TODO -> Write code to request 3 integers of the same value.  If the user
+#         enters 3 integers that are not the same, ask them again until they get it right.
+
+
+
+# TODO -> Without referring to the lesson notes or Internet, implement a simple
+#         guessing game.  You can use random.randint(1, 100) to generate a number
+#         to guess.
+#
+# Sample output:
+#
+# Enter guess: 50
+# too low
+# Enter guess: 60
+# too high
+# Enter guess: 55
+# correct in 3 guesses
+
